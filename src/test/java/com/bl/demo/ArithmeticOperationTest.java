2a219c6b190d7f0e4a3dbbac8ec8a73416e32bfa
Tushar-W <twankhede95@gmail.com>
2020-04-20 14:16:34 +0530

    ADDED:tested subtraction of two number

--- src/test/java/com/bl/demo/ArithmeticOperationTest.java
+++ src/test/java/com/bl/demo/ArithmeticOperationTest.java
@@ -10,4 +10,11 @@ public class ArithmeticOperationTest {
         int addition = arithmeticOperation.additionOfNumber(2,3);
         Assert.assertEquals(5,addition);
     }
+
+    @Test
+    public void givenTwoInteger_WhenSubtract_ShouldReturnSubtraction() {
+        ArithmeticOperation arithmeticOperation = new ArithmeticOperation();
+        int subtraction = arithmeticOperation.subtractionOfNumber(3,2);
+        Assert.assertEquals(1,subtraction);
+    }
 }
